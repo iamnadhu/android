576775f7b7824fbf06fbdc90ba07714807e59dfa
Nadhu <nadhu014@gmail.com>
2020-09-02 13:32:13 +0530

    My First App Added

--- my-first-app/app/src/main/java/com/nadhu014/myfirstapp/MainActivity.java
+++ my-first-app/app/src/main/java/com/nadhu014/myfirstapp/MainActivity.java
@@ -1,14 +1,38 @@
 package com.nadhu014.myfirstapp;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvHeading;
+    Button btnReset, btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvHeading = findViewById(R.id.tv_heading);
+        btnReset = findViewById(R.id.btn_reset);
+        btnSubmit = findViewById(R.id.btn_submit);
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvHeading.setText("My First App!");
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvHeading.setText("Welcome to Android.");
+            }
+        });
     }
 }
